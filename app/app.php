048086690988cd1e394344fdaf03da86de907dd1
vjLarios <vjosuee05@gmail.com>
2025-05-21 23:51:52 -0600

    Agregar archivos del framework

---
+++ app/app.php
@@ -0,0 +1,16 @@
+<?php
+// app/app.php
+
+// 1. Carga configuración
+require __DIR__ . '/config.php';
+
+// 2. Carga clases principales
+require __DIR__ . '/classes/Autoloader.php';
+require __DIR__ . '/classes/Router.php';
+
+// 3. Arranca el autoloader
+$autoloader = new Autoloader();
+$autoloader->register();
+
+// 4. Instancia el router y defínelo como global
+$router = new Router();
